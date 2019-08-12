7a95b9d91f21c7965f71d1cf5375727e2a8dfa7c
rs <reissedler@gmail.com>
2019-08-12 16:46:19 +0100

    a

--- src/main/java/com/rsedlr/WikiRace/search.java
+++ src/main/java/com/rsedlr/WikiRace/search.java
@@ -1,9 +1,9 @@
 
-public static int[] search(Database database, int startID, int endID) {
+public static int[][] search(Database database, int startID, int endID) {
 
   if (startID == endID){
     return [[startID]]
   }
 
-  
+
 }
